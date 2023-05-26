01ddff1c7ba22b7b064addad365d1025370d07e0
JD <64992493+hiringManager@users.noreply.github.com>
2023-05-26 06:43:27 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -12,8 +12,26 @@ mkdir -p extracted
 
 ls "$WORKING_DIRECTORY"
 
+# Is binwalk even needed? If we purely do the "ADD" with the dockerfile, we may be able to mount it that way
+
+# need to put fucking case statements here
 for file in *.zip; do
     unzip "$file" -d ./extracted
+done
+
+# do firmware blobs use fucking elf?
+for file in *.hex; do
+    continue
+done
+
+# fucking tar
+for file in *.tar; do
+    tar xvf "$file" ./extracted
+done
+
+# binary
+for file in *.bin; do
+    binwalk "$file" ./extracted
 done
 
 binwalk -Me ./*
@@ -48,4 +66,30 @@ rm -r output
 rm -r extracted
 rm -r *.extracted
 
+arch=$(lscpu | grep Architecture | awk '{print $2}')
+
+case $arch in
+    x86_64)
+        # Code to execute when architecture is x86_64
+        echo "x86_64 architecture detected"
+
+        # Additional check for mips
+        if lscpu | grep -q mips; then
+            # Code to execute when mips is detected
+            echo "mips architecture detected"
+        fi
+
+        # Additional check for arm
+        if lscpu | grep -q arm; then
+            # Code to execute when arm is detected
+            echo "arm architecture detected"
+        fi
+        ;;
+    *)
+        # Code to execute for other architectures
+        echo "Other architecture detected"
+        ;;
+esac
+
 # docker build -t test .
+# docker run -it test
